8d3e5f36b61d21d961b2725e59872e9c1dc931c8
davidrodjen <davidrodriguezjenkins@gmail.com>
2019-12-03 10:59:50 -0800

    QuerySelector updates

---
+++ scripts/musictone.js
@@ -0,0 +1,5 @@
+const synth = new webkitConvertPointFromPageToNode.AMSynth().toMaster()
+
+document.querySelector('tone-keyboard').addEventListener('noteon', e =>{})
+
+document.querySelector('tone-keyboard').addEventListener('noteoff', e => {})
