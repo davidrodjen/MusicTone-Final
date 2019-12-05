ff1d22aa1f07aa5cfd8529f1ba017ca15f67f3cd
davidrodjen <davidrodriguezjenkins@gmail.com>
2019-12-04 17:00:26 -0800

    Created and implimented unique piano

--- scripts/musictone.js
+++ scripts/musictone.js
@@ -26,32 +26,32 @@
 //.toMaster connects it to the speakers
 const synth = new Tone.AMSynth().toMaster()
 
-synth.triggerAttackRelease('C4', '8n');
-const notes = [
-  'C4', 'E4', 'G4',
-  'C5', 'E5', 'G5'
-];
+// synth.triggerAttackRelease('C4', '8n');
+// const notes = [
+//   'C4', 'E4', 'G4',
+//   'C5', 'E5', 'G5'
+// ];
 
-var index = 0
+// var index = 0
 
-Tone.Transport.scheduleRepeat(time => {
-  repeat(time);
-}, '8n');
+// Tone.Transport.scheduleRepeat(time => {
+//   repeat(time);
+// }, '8n');
 
-console.log(Tone.Transport.bm.value);
+// console.log(Tone.Transport.bm.value);
 
-//%notes.length will cause a repeat
-function repeat(time) {
-  let note = notes[index % notes.length];
-  synth.triggerAttackRelease(note, '8n', time);
-  index++;
-}
+// //%notes.length will cause a repeat
+// function repeat(time) {
+//   let note = notes[index % notes.length];
+//   synth.triggerAttackRelease(note, '8n', time);
+//   index++;
+// }
 
-tone.Transport.start();
+// tone.Transport.start();
 
-setTimeout(() => {
-  Tone.Transport.stop();
-}, 5000)
+// setTimeout(() => {
+//   Tone.Transport.stop();
+// }, 5000)
 
 
 ///////////////////////////
